89bbad658be743be013ce8dead6a42e5a395c066
jiawenhao <jiawenhao@fcbox.com>
2022-06-16 18:51:52 +0800

    初始化项目

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+        <el-row class="home-container">
+            <el-col :span="4" class="home-container">
+                <div class="home-container-tree">第一层笔记</div>
+            </el-col>
+            <el-col :span="4" class="home-container">
+                <div class="grid-content bg-purple-light">笔记列表</div>
+            </el-col>
+            <el-col :span="16" class="home-container">
+                <div class="grid-content bg-purple-light">编辑页</div>
+            </el-col>
+        </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-container{
+    height: calc(97vh);
+}
+.home-container-tree{
+    height: 100%;
+    background: #222530;
+}
+
+</style>>
